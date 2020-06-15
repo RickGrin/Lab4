08928894898b8edebd02f22d14ac7c936ca31822
Сергей <56985915+RickGrin@users.noreply.github.com>
2020-06-15 23:26:40 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include "iostream"
+#include <string.h>
+#include <conio.h>
+using namespace std;
+void Sort(int A[], int N, int i)
+{
+    if (i < N)
+    {
+        int min = A[i], index = i;
+        for (int j = i + 1; j < N; j++)
+            if (A[j] < min) min = A[j], index = j;
+        A[index] = A[i];
+        A[i] = min;
+        i++;
+        Sort(A, N, i);
+    }
+}
+int main()
+{
+    int i = 0, N = 6;
+
+    int* mas = new int[6]{ 9, 7, 5, 3, 1 };
+    Sort(mas, N, i);
+    for (int j = 0; j < N; j++)
+        cout << mas[j] << "  ";
+    _getch();
+    delete mas;
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int member(int memb, int dif, int n)
+{
+    if (n) memb = dif + member(memb, dif, n - 1);
+    return memb;
+}
+
+
+int main()
+{
+    int N = 3;
+    int differ = 7;//разность
+    int first = 1;
+    cout << member(first, differ, N - 1) << endl;
+    return 0;
+}
